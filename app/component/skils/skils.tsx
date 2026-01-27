b04fc17c2570ab4b054b7c2531c594f3fa1dbe41
s1mple-User <u3905173@gmail.com>
2026-01-27 14:25:40 +0500

    Correct 'Algartims' to 'Algorithms' in skills
    Fix typo in skills list.

--- app/component/skils/skils.tsx
+++ app/component/skils/skils.tsx
@@ -78,7 +78,7 @@ export default function Skils() {
                ● Atomic Design <br /> 
                ● Микрофронты  <br />
                ● Микросервисы <br />
-              ● Algartims  & Data Structur
+              
              </p>
          </div>
 
